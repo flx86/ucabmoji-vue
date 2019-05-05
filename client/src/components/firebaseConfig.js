0f6057a0eccaf3221e655e122d9354853d6250e8
Felix Lopez <45021056+flx86@users.noreply.github.com>
2019-05-05 15:36:41 -0400

    :hammer: Fixing API key issues

--- client/src/components/firebaseConfig.js
+++ client/src/components/firebaseConfig.js
@@ -1,10 +1,10 @@
-
+// enter to firebase dashboard to get this config
 export default {
-  apiKey: "AIzaSyDaNFIny3JMgW5Ftca3awcjprTIwBY5ybI",
-  authDomain: "proyecto-ucabmoji.firebaseapp.com",
-  databaseURL: "https://proyecto-ucabmoji.firebaseio.com",
-  projectId: "proyecto-ucabmoji",
-  storageBucket: "proyecto-ucabmoji.appspot.com",
-  messagingSenderId: "248422988570"
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: ""
 };
   
